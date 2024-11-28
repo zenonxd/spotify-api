c2eacc740ef0739f384dad34a968a933e44df324
zenonxd <olavomoreira99@gmail.com>
2024-11-28 16:25:20 -0300

    feat: testing Open Feign

---
+++ src/main/java/com/moreira/spotifyapi/client/GoogleClient.java
@@ -0,0 +1,14 @@
+package com.moreira.spotifyapi.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "GoogleClient",
+        url = "https://google.com"
+)
+public interface GoogleClient {
+
+    @GetMapping
+    String helloWorld();
+}
